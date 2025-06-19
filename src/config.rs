41410e190bd06423e4bfc8c8ebfd224293d9eef0
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2025-06-19 06:18:43 +0200

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.0.9"];
+pub const RS_PUB_KEY: &str = "u6yzIZOgoNMQJOGJ72v8w7xd8Wr6pMplMXOWV9BY4H4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
