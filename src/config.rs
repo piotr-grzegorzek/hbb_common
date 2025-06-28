af409aac67e7282c93dd792eb81367ce7123ccc4
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2025-06-29 01:34:30 +0200

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.0.9"];
-pub const RS_PUB_KEY: &str = "u6yzIZOgoNMQJOGJ72v8w7xd8Wr6pMplMXOWV9BY4H4=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["palomar.cc"];
+pub const RS_PUB_KEY: &str = "NVJ5bFHoLuSnWyyI3laJPkQljGH4yWCGFsM5xLyaTmY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
